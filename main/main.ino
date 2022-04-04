97b85dc7912398db8ee4a435fa9b9f055ce26e7a
GIHAA <gihansudeeparandimal@gmail.com>
2022-04-05 03:59:37 +0530

    final

--- main/main.ino
+++ main/main.ino
@@ -6,21 +6,22 @@
 #define FIREBASE_AUTH "zF8jnuNaxVICYzlTxLYyYbMPXySSgSQEJJD6M8pB"
 #define WIFI_SSID "Carpark"
 #define WIFI_PASSWORD "12344567"
-#define block_dis 7 // cm
+#define block_dis 6 // cm
 #define min_dis 15 // cm
 #define car_dis 7 // cm
 #define TRIGGER 12 // d6
 #define ECHO 14 // d5
 #define TRIGGER2 5 // d1
 #define ECHO2 4 // d2
-#define TRIGGER3 0 // d3
+#define TRIGGER3 0 // d3 entrance
 #define ECHO3 13 // d7
-#define pin 15 //d8
 
-//output d4 trigger - 2
-//input rx eco - 3
+#define TRIGGER4 2 // d4 exit
+#define ECHO4 3 // rx
+#define pin 15 // d8
 
 Servo servo;
+bool check;
 
 void setup() {
   
@@ -32,11 +33,12 @@ void setup() {
   pinMode(ECHO2, INPUT);
   pinMode(TRIGGER3, OUTPUT);
   pinMode(ECHO3, INPUT);
+  pinMode(TRIGGER4, OUTPUT);
+  pinMode(ECHO4, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
   servo.attach(pin);
 
 
-  // connect to wifi.
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("connecting");
   while (WiFi.status() != WL_CONNECTED) {
@@ -48,7 +50,7 @@ void setup() {
   Serial.println(WiFi.localIP());
   
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-
+  
 }
 
 
@@ -59,6 +61,7 @@ void loop() {
   bool sen_2_blocked = false;
   bool car_2_parked = false;
   bool incoming_car = false;
+  bool exiting_car = false;
 /************************************************/
   long duration, distance;
   digitalWrite(TRIGGER, LOW);
@@ -96,6 +99,21 @@ void loop() {
   duration3 = pulseIn(ECHO3, HIGH);
   
   distance3 = (duration3/2) / 29.1; 
+
+ /************************************************/
+  long duration4, distance4;
+  digitalWrite(TRIGGER4, LOW);
+  delayMicroseconds(2); 
+  
+  digitalWrite(TRIGGER4, HIGH);
+  delayMicroseconds(10); 
+  
+  digitalWrite(TRIGGER4, LOW);
+  duration4 = pulseIn(ECHO4, HIGH);
+  
+  distance4 = (duration4/2) / 29.1; 
+
+  /*********************debug**********************/
   
   Serial.print("debug - distance1 :");
   Serial.println(distance);
@@ -103,6 +121,8 @@ void loop() {
   Serial.println(distance2);
   Serial.print("debug - distance3 :");
   Serial.println(distance3);
+  Serial.print("debug - distance4 :");
+  Serial.println(distance4);
   Serial.println("...........");
   Serial.println("...........");
   Serial.println("...........");
@@ -110,16 +130,25 @@ void loop() {
 
   /*******************logic*********************/
 
-    if( distance < block_dis )
+    if( distance < block_dis ){
         sen_1_blocked = true;
-    else
+        Firebase.setBool("sen1_blocked", true);
+    }
+    else{
         sen_1_blocked = false;
+        Firebase.setBool("sen1_blocked", false);
+    }
 
-    if( distance2 < block_dis )
-        sen_1_blocked = true;
-    else
-        sen_1_blocked = false;
+    if( distance2 < block_dis ){
+        sen_2_blocked = true;
+        Firebase.setBool("sen2_blocked", true);
+    }
+    else{
+        sen_2_blocked = false;
+        Firebase.setBool("sen2_blocked", false);
+    }
 
+ 
 
 
     if( distance < min_dis )
@@ -138,12 +167,24 @@ void loop() {
     else
         incoming_car = false;
 
+    if( distance4 < min_dis )
+        exiting_car = true;
+
     /*******************sending logic****************/
 
     if( incoming_car == true){
       Firebase.setBool("incoming_car", true);
-      if( car_1_parked == false && car_2_parked == false )
-        servo.write(90);
+        if( car_1_parked == false || car_2_parked == false ){
+          for(int i = 0; i < 120; i++){
+            servo.write(i);
+            delay(7);
+          }
+          delay(2000);
+           for(int i = 120; i >= 0; i--){
+             servo.write(i);
+              delay(7);
+          }
+        }
     }
     else{
       Firebase.setBool("incoming_car", false);
@@ -162,9 +203,29 @@ void loop() {
     else
       Firebase.setBool("Car_parked_spot2", false);
 
+    if( exiting_car == true ){
+      Firebase.setBool("Car_exiting", true);
+      
+      for(int i = 0; i < 120; i++){
+        servo.write(i);
+        delay(7);
+      }
+     
+        delay(2000);
+      for(int i = 120; i >= 0; i--){
+        servo.write(i);
+        delay(7);
+      }
+      
+    }
+    else{
+      Firebase.setBool("Car_exiting", false);
+      servo.write(0);    
+    }
   Firebase.setFloat("Distance", distance);
   Firebase.setFloat("Distance2", distance2);
   Firebase.setFloat("Distance3", distance3);
+  Firebase.setFloat("Distance4", distance4);
 
   // handle error
   if (Firebase.failed()) {
